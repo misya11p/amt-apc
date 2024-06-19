696f09283d9899f8cec7e0489cd61a2c06c3df33
komiya <90894519+misya11p@users.noreply.github.com>
2024-06-20 02:37:43 +0900

    use pipeline in wav2feature

--- data/_utils.py
+++ data/_utils.py
@@ -1,31 +1,8 @@
-import json
-import torch
-import torchaudio
-import torchaudio.functional as F
+from models import Pipeline
 
 
-with open("models/config.json", "r") as f:
-    CONFIG = json.load(f)["data"]
-
-
-def load_wav(path_input):
-    y, sr = torchaudio.load(path_input)
-    y = torch.mean(y, dim=0)
-    y = F.resample(y, sr, CONFIG["feature"]["sr"])
-    return y
+PIPELINE = Pipeline(skip_load_model=True)
 
 
 def wav2feature(path_input):
-    y, _ = torchaudio.load(path_input)
-    transform = torchaudio.transforms.MelSpectrogram(
-        sample_rate=CONFIG["feature"]["sr"],
-        n_fft=CONFIG["feature"]["fft_bins"],
-        win_length=CONFIG["feature"]["window_length"],
-        hop_length=CONFIG["feature"]["hop_sample"],
-        pad_mode=CONFIG["feature"]["pad_mode"],
-        n_mels=CONFIG["feature"]["mel_bins"],
-        norm="slaney",
-    )
-    feature, = transform(y)
-    feature = (torch.log(feature + CONFIG['feature']['log_offset'])).T
-    return feature
+    return PIPELINE.wav2feature(path_input)
