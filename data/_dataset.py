744d635154d2d52513cecac021ef68709ad9d5da
komiya <90894519+misya11p@users.noreply.github.com>
2024-08-15 01:40:30 +0900

    dataset update

--- data/_dataset.py
+++ data/_dataset.py
@@ -1,68 +1,53 @@
 import json
-import random
+from pathlib import Path
 
+import numpy as np
 import torch
-from torch.utils.data import Dataset, DataLoader
-
-from ._utils import preprocess_feature
+from torch.utils.data import Dataset
 
 
+DIR_NAME_LABEL = "label/"
+DIR_NAME_SPEC = "spec/"
 with open("models/config.json", "r") as f:
     CONFIG = json.load(f)["data"]
+with open("data/piano2orig.json", "r") as f:
+    PIANO2ORIG = json.load(f)
+with open("data/style_vector.json", "r") as f:
+    STYLE_VECTOR = json.load(f)["style_vector"]
 
 
-class SyncedPianoDataset(Dataset):
-    def __init__(self, path_spec, n_frames):
-        self.n_frames = n_frames
-        spec = torch.load(path_spec)
-        spec_orig = spec[0]
-        spec_pianos = spec[1:]
-
-        spec_orig = preprocess_feature(spec_orig)
-        spec_orig_split = self._split(spec_orig)
-
-        data = []
-        for spec_piano in spec_pianos:
-            spec_piano = preprocess_feature(spec_piano)
-            spec_piano_split = self._split(spec_piano)
-            for spec_orig, spec_piano in zip(spec_orig_split, spec_piano_split):
-                data.append((spec_orig, spec_piano))
-            data = data[:-1]
-        self.data = data
+class PianoCoversDataset(Dataset):
+    def __init__(self, dir_dataset: str):
+        self.dir_dataset = Path(dir_dataset)
+        self.data = list((self.dir_dataset / DIR_NAME_LABEL).glob("*.npz"))
 
     def __len__(self):
         return len(self.data)
 
     def __getitem__(self, idx):
-        return self.data[idx]
+        path = self.data[idx]
+        label = np.load(path)
+        spec, sv = self.get_spec_sv(path.stem)
 
-    def _split(self, spec):
-        specs = []
-        for i in range(0, len(spec), self.n_frames):
-            spec_block = (spec[i:i+CONFIG["input"]["margin_b"]+self.n_frames+CONFIG["input"]["margin_f"]]).T
-            specs.append(spec_block)
-        return specs
+        spec = torch.from_numpy(spec).float()
+        sv = torch.tensor(sv).float()
+        onset = torch.from_numpy(label["onset"]).float()
+        offset = torch.from_numpy(label["offset"]).float()
+        mpe = torch.from_numpy(label["mpe"]).float()
+        velocity = torch.from_numpy(label["velocity"])
 
+        return spec, sv, onset, offset, mpe, velocity
 
-class SyncedPianos:
-    def __init__(self, dir_specs, n_frames, batch_size=1):
-        self.data = list(dir_specs.glob("*.pth"))
-        random.shuffle(self.data)
-        self.n_frames = n_frames
-        self.batch_size = batch_size
+    def get_spec_sv(self, stem: str):
+        split = stem.split("_")
+        n_segment = split[-1]
+        id_piano = "_".join(split[:-1])
 
-    def __iter__(self):
-        for i in range(len(self)):
-            yield self[i]
+        id_orig = PIANO2ORIG[id_piano]
+        fname_orig = f"{id_orig}_{n_segment}.npy"
+        path_orig = self.dir_dataset / DIR_NAME_SPEC / fname_orig
+        spec = np.load(path_orig)
 
-    def __len__(self):
-        return len(self.data)
+        sv = STYLE_VECTOR[id_piano]
 
-    def __getitem__(self, idx):
-        dataset = SyncedPianoDataset(self.data[idx], self.n_frames)
-        dataloader = DataLoader(
-            dataset,
-            batch_size=self.batch_size,
-            shuffle=True,
-        )
-        return dataloader
+        return spec, sv
