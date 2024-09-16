8b9eef0ae35112c3334f603497b3f0e7d6ea97fd
komiya <90894519+misya11p@users.noreply.github.com>
2024-09-16 23:29:12 +0900

    dictを継承

--- utils/_config.py
+++ utils/_config.py
@@ -1,21 +1,18 @@
 from pathlib import Path
 import json
-from typing import Dict
+from typing import Dict, Any
 
 
-class Config:
+class Config(dict):
     def __init__(self, config: Dict):
-        self.config = config
+        super().__init__(config)
 
-    def __getattr__(self, name: str) -> Dict:
-        value = self.config[name]
+    def __getattr__(self, name: str) -> Dict | Any:
+        value = self[name]
         if isinstance(value, dict):
             return Config(value)
         else:
             return value
-
-    def __repr__(self) -> str:
-        return self.config.__repr__()
 
 
 dir_this = Path(__file__).parent.parent
