a951e8d026f489050033c080f16aeabb89d541d9
komiya <90894519+misya11p@users.noreply.github.com>
2024-09-21 11:55:00 +0900

    クラス名変えたり

--- utils/_config.py
+++ utils/_config.py
@@ -3,20 +3,27 @@ import json
 from typing import Dict, Any
 
 
-class Config(dict):
+class CustomDict(dict):
     def __init__(self, config: Dict):
         super().__init__(config)
 
     def __getattr__(self, name: str) -> Dict | Any:
         value = self[name]
         if isinstance(value, dict):
-            return Config(value)
+            return CustomDict(value)
         else:
             return value
+
+    def __getitem__(self, key: Any) -> Any:
+        item = super().__getitem__(key)
+        if isinstance(item, dict):
+            return CustomDict(item)
+        else:
+            return item
 
 
 root = Path(__file__).parent.parent
 path_config = root / "config.json"
 with open(path_config, "r") as f:
     config_json = json.load(f)
-config = Config(config_json)
+config = CustomDict(config_json)
