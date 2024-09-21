93287a8b3bf2d94eb98f7c8be4595d3ff6728ae9
komiya <90894519+misya11p@users.noreply.github.com>
2024-09-21 12:20:00 +0900

    cover.pyでaudio化まで

--- eval/cover.py
+++ eval/cover.py
@@ -1,58 +1,80 @@
-import sys; sys.path.append("./")
-import argparse
 from pathlib import Path
-import json
-import random
+import sys
+import argparse
+
+ROOT = Path(__file__).parent.parent
+sys.path.append(str(ROOT))
 
 import torch
+from midi2audio import FluidSynth
 from tqdm import tqdm
 
 from models import Pipeline
+from data import SVSampler
+from utils import info
 
 
 DEFAULT_DEVICE = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-with open("models/config.json", "r") as f:
-    CONFIG = json.load(f)
-PATH_PC = CONFIG["default"]["pc"]
-SV_DIM = CONFIG["model"]["sv_dim"]
+sv_sampler = SVSampler()
 
 
 def main(args):
-    dir_input = Path(args.dir_original)
-    dir_output = Path(args.dir_output)
+    dir_output = ROOT / args.dir_output
     dir_output.mkdir(exist_ok=True)
-    with open(args.path_params, "r") as f:
-        params = json.load(f)
-
     device = torch.device(args.device) if args.device else DEFAULT_DEVICE
-    pipeline = Pipeline(path_model=args.model, device=device, sv_dim=SV_DIM)
 
-    with open(args.path_style_vector) as f:
-        style_vectors = list(json.load(f)["style_vector"].values())
+    # Create MIDI files from WAV files
+    midis = cover(
+        dir_output, args.path_model, device, args.with_sv, args.no_load
+    )
 
-    songs = list(dir_input.glob("*.wav"))
+    # Convert MIDI files to audio files
+    midi2audio(midis, args.sound_font)
+
+
+def cover(dir_output, path_model, device, with_sv, no_load):
+    pipeline = Pipeline(
+        path_model=path_model,
+        device=device,
+        with_sv=with_sv,
+        no_load=no_load,
+        device=device
+    )
+
+    songs = info.get_ids("test", orig=True)
     songs = sorted(songs)
+    midis = []
     for song in tqdm(songs):
-        path_midi = dir_output / f"{song.stem}.mid"
-        sv = torch.tensor(random.choice(style_vectors)).to(device)
+        path_input = info.id2path(song, orig=True)
+        path_output = dir_output / f"{song}.mid"
+        sv = sv_sampler.random()
         pipeline.wav2midi(
-            path_input=str(song),
-            path_output=str(path_midi),
+            path_input=str(path_input),
+            path_output=str(path_output),
             sv=sv,
-            thred_onset=params["threshold"]["onset"],
-            thred_offset=params["threshold"]["offset"],
-            thred_mpe=params["threshold"]["mpe"],
-            min_length=params["min_duration"],
         )
+        midis.append(path_output)
+    return midis
+
+
+def midi2audio(midis, sound_font):
+    if sound_font:
+        fs = FluidSynth(sound_font=sound_font)
+    else:
+        fs = FluidSynth()
+
+    for midi in midis:
+        path_save = midi.with_suffix(".wav")
+        fs.midi_to_audio(str(midi), str(path_save))
 
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
-    parser.add_argument("-i", "--dir_original", type=str, default="./eval/data/songs")
-    parser.add_argument("-o", "--dir_output", type=str, default="./eval/data/cover_midi")
-    parser.add_argument("--model", type=str, default=None)
+    parser.add_argument("--dir_output", "-o", type=str, default="eval/data/")
+    parser.add_argument("--path_model", type=str, default=None)
     parser.add_argument("--device", type=str, default=None)
-    parser.add_argument("--path_params", type=str, default="./eval/params.json")
-    parser.add_argument("--path_style_vector", type=str, default="./data/style_vector.json")
+    parser.add_argument("--with_sv", action="store_true")
+    parser.add_argument("--no_load", action="store_true")
+    parser.add_argument("--sound_font", type=str, default=None)
     args = parser.parse_args()
     main(args)
